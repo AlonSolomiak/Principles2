663792f48b9ee89f9da944c529ed51024458d713
Alony203 <tkuiso203@gmail.com>
2025-11-05 17:06:15 +0200

    fixed some bugs in graphs, added max+min fluxes

--- Lab1/code2.py
+++ Lab1/code2.py
@@ -6,8 +6,6 @@ def get_table_from_file():
     with open("C:\\Users\\tkuis\\Documents\\Github\\Princples2\\Principles2\\Lab1\\temp_2D.txt", "r") as f:
         return [[float(num) for num in line.split(" ")] for line in f]
 
-
-
 def find_max(A):
     max = A[0][0]
     for i in range(len(A)):
@@ -16,7 +14,6 @@ def find_max(A):
                 max = A[i][j]
     
     return max
-
 
 def find_min(A):
     min = A[0][0]
@@ -27,16 +24,14 @@ def find_min(A):
     
     return min
 
-
-def print_table(A):
-    extent = 0, len(A), 0, len(A[0])
+def print_table(A, title, xlabel, ylabel, label):
+    extent = 0, len(A)-1, 0, len(A[0])-1
     plt.imshow(A, cmap=plt.cm.plasma, interpolation='bicubic', extent=extent, origin='lower')
-    plt.title("Targil 2")
-    plt.colorbar(cmap='plasma', label='T[K]')
-    plt.xlabel('X [cm]')
-    plt.ylabel('y [cm]')
+    plt.title(title)
+    plt.colorbar(cmap='plasma', label=label)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.show()
-
 
 def forward_diference(f1, f2, dx):
     return (f2-f1)/dx
@@ -49,35 +44,35 @@ def central_diference(f0, f2, dx):
 
 def find_flux_X(k, A):
     flux_X = [[] for row in range(len(A))]
-    for i in range(len(A)):
-        flux_X[i].append(-1 * k * forward_diference(A[i][0], A[i][1], dx))
-        for j in range(1, len(A[i]) - 1):
-            flux_X[i].append(-1 * k * central_diference(A[i][j-1], A[i][j+1], dx))
-        flux_X[i].append(-1 * k * backward_diference(A[i][j-2], A[i][j-1], dx))
+    for row in range(len(A)):
+        flux_X[row].append(-1 * k * forward_diference(A[row][0], A[row][1], dx))
+        for col in range(1, len(A[row]) - 1):
+            flux_X[row].append(-1 * k * central_diference(A[row][col-1], A[row][col+1], dx))
+        flux_X[row].append(-1 * k * backward_diference(A[row][len(A[row])-2], A[row][len(A[row])-1], dx))
 
     return flux_X
 
-
 def find_flux_Y(k,A):
     flux_Y = [[] for row in range(len(A))]
-    for i in range(len(A[0])):
-        print(flux_Y)
-        flux_Y[0].append(-1 * k * forward_diference(A[0][i], A[1][i], dx))
-        for j in range(1, len(A) - 1):
-            flux_Y[j].append(-1 * k * central_diference(A[j-1][i], A[j+1][i], dx))
-        flux_Y[len(A)-1].append(-1 * k * backward_diference(A[len(A)-2][i], A[len(A)-1][i], dx))
+    for col in range(len(A[0])):
+        flux_Y[0].append(-1 * k * forward_diference(A[0][col], A[1][col], dx))
+        for row in range(1, len(A) - 1):
+            flux_Y[row].append(-1 * k * central_diference(A[row-1][col], A[row+1][col], dx))
+        flux_Y[len(A)-1].append(-1 * k * backward_diference(A[len(A)-2][col], A[len(A)-1][col], dx))
 
     return flux_Y
 
 
 
-table = get_table_from_file()
-print(table)
-print(find_max(table))
-print(find_min(table))
-print_table(table)
-Flux_x_table = find_flux_X(K, table)
-print_table(Flux_x_table)
-Flux_y_table = find_flux_Y(K, table)
-print(Flux_y_table)
-print_table(Flux_y_table)
+T_table = get_table_from_file()
+print("Max T: " + str(find_max(T_table)))
+print("Min T: " + str(find_min(T_table)))
+print_table(T_table, "Section C - Temprature Distribution", "X (cm)", "Y (cm)", "T [K]")
+Flux_x_table = find_flux_X(K, T_table)
+print_table(Flux_x_table, "Section D - Distribution of Heat Flux in X direction", "X (cm)", "Y (cm)", "Flux_x [W/m^2]")
+Flux_y_table = find_flux_Y(K, T_table)
+print_table(Flux_y_table, "Section D - Distribution of Heat Flux in Y direction", "X (cm)", "Y (cm)", "Flux_y [W/m^2]")
+print("Max Flux X: " + str(find_max(Flux_x_table)))
+print("Min Flux X: " + str(find_min(Flux_x_table)))
+print("Max Flux Y: " + str(find_max(Flux_y_table)))
+print("Min Flux Y: " + str(find_min(Flux_y_table)))
